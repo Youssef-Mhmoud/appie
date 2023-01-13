03887f4f24c75f43fb1b40e93c97372d691cc3db
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2023-01-13 23:25:52 +0200

    make navbar sticky

--- js/script.js
+++ js/script.js
@@ -13,8 +13,31 @@ btnClosesEl.addEventListener("click", (e) => {
 // Get nav links
 const linksNavEl = document.querySelectorAll(".nav-list .btn-link");
 
-linksNavEl.forEach(e => {
-  e.addEventListener('click', link => {
-    body.classList.remove('open')
-  })
-})
+linksNavEl.forEach((e) => {
+  e.addEventListener("click", (link) => {
+    body.classList.remove("open");
+  });
+});
+
+// Make navbar sticky
+const sectionHeroEl = document.querySelector(".hero-section");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-68px",
+  }
+);
+
+obs.observe(sectionHeroEl);
